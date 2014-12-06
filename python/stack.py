f68bc35b3194a25832dc420df136d26d6ad5b2c6
ugur donmez <donmez@metu.edu.tr>
2014-12-06 11:45:33 +0200

    Stack implementation with python Initilization

---
+++ python/stack.py
@@ -0,0 +1,12 @@
+import linkend_list
+
+l = linkend_list.Linked_list()
+
+l.add_node_head(1)
+l.add_node_head(2)
+l.add_node_head(3)
+l.add_node_head(4)
+l.add_node_head(5)
+
+print "a"
+l.list_print()
