719768ddf4ddd9fd4c429ef82c9d6296589f7c11
ugur <ugurdonmez87@gmail.com>
2014-12-07 14:15:33 +0200

    Python Stack

--- python/stack.py
+++ python/stack.py
@@ -1,6 +1,43 @@
-import linkend_list
+# execfile("./linked_list.py")
 
-l = linkend_list.Linked_list()
+from linked_list import Linked_list
+
+class Stack:
+	def __init__(self):
+		self.list = Linked_list()
+		self.size = 0
+	
+	def push(self,data):
+		self.list.add_node_head(data)
+		self.size += 1
+	
+	def pop(self):
+		if self.isEmpty:
+			return None
+		else :
+			self.size -= 1
+			data = self.top()
+			self.list.delete_node(data)
+			return data
+			
+	
+	def top(self):
+		if self.isEmpty:
+			return None
+		else:
+			return self.list.get_head.data
+	
+	def isEmpty(self):
+		if self.size == 0:
+			return True
+		else:
+			return False
+			
+
+'''
+s = Stack()
+
+l = Linked_list()
 
 l.add_node_head(1)
 l.add_node_head(2)
@@ -10,3 +47,4 @@ l.add_node_head(5)
 
 print "a"
 l.list_print()
+'''
