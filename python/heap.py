5a57cdb42379e8f68cab3b410cbe7bc26b2bf228
ugur donmez <donmez@metu.edu.tr>
2014-12-09 16:49:38 +0200

    Python Heap init

---
+++ python/heap.py
@@ -0,0 +1,21 @@
+class Heap:
+   def __init__(self):
+      self.array = []
+   
+   
+   def create-heap(self):  #create an empty heap
+   
+   def heapify(self): #create a heap out of given array of elements
+    
+   def find_max(self):
+      
+   def delete_max(self):
+  
+   def insert(self, data):
+   
+   def merge(self,heap):
+      
+   def size(self):
+   
+   def isEmpty(self):
+
